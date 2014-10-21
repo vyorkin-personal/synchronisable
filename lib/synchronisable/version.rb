c30a41a7764078b6e1e838ea759596a8afeaf2cd
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2014-10-21 21:11:27 +0400

    bump version to 1.2.0

--- lib/synchronisable/version.rb
+++ lib/synchronisable/version.rb
@@ -1,8 +1,8 @@
 module Synchronisable
   module VERSION
     MAJOR = 1
-    MINOR = 1
-    PATCH = 9
+    MINOR = 2
+    PATCH = 0
     SUFFIX = nil
 
     STRING = [MAJOR, MINOR, PATCH, SUFFIX].compact.join('.')
